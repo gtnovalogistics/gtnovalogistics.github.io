b7a41e5a1dae7df4722276a1ac4f3ae5b8ad754a
developer@gtnovatechnology.ca <developer@gtnovatechnology.ca>
2024-09-28 16:26:16 -0400

    cleaned up header

---
+++ components/header/header.js
@@ -0,0 +1,27 @@
+import "./socials-contacts.js";
+import "./main-header.js";
+
+const template = document.createElement('template');
+template.innerHTML = `
+    <style>
+        :host {
+  
+    
+        }
+    </style>
+    
+    <section>
+        <wc-socials-contacts class="socials-contacts"></wc-socials-contacts>
+        <wc-main-header></wc-main-header>
+    </section>
+`;
+
+
+
+customElements.define('wc-header', class extends HTMLElement {
+    constructor() {
+        super();
+        const shadow = this.attachShadow({ mode: 'open' });
+        shadow.appendChild(template.content.cloneNode(true));        
+    }
+});
